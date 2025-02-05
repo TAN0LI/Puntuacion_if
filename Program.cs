0692bd971633055a6ae066bf4d501a6897a9f571
ICEL <ICEL@DESKTOP-B8MFEUD>
2025-02-05 07:45:57 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puntuacion_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Ingresa tu puntuacion");
+            double p = double.Parse(Console.ReadLine());
+            if(p >= 100)
+            {
+                System.Console.WriteLine("ALTO");
+            }
+            else
+            {
+                System.Console.WriteLine("BAJO");
+            }
+        }
+    }
+}
